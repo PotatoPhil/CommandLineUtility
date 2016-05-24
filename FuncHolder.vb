80d7d75dec0eecbbf6f3d9dbb9c2beada906113e
Alam Romo <romoalamn@gmail.com>
2016-05-23 21:35:42 -0700

    These classes run the CommandLineUtility
    WARNING: this will not work in its current state! The CmdLib is
    necessary for this to run, as well as for any plugins that might be
    developed

---
+++ FuncHolder.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Text
+Imports System.IO
+Imports CommandLib
+Module FuncHolder
+    Public Sub DoForHelp(Args As List(Of String))
+        If (Args.Count = 0) Then
+            LogLevelDebug("Recognised Input as help")
+            LogLevelDebug("Creating StringBuilder for help printout")
+            Dim ToEcho As StringBuilder = New StringBuilder("Printing Help")
+            LogLevelDebug("Looping through CommandGroups")
+            For Each CommandGroup In Parser.CommandList
+                ToEcho.Append(CommandGroup.Name)
+                ToEcho.Append(" : ")
+                ToEcho.Append(CommandGroup.Description)
+                ToEcho.Append(" and has variations with ")
+                LogLevelDebug("Looping Through commands in " & CommandGroup.Name)
+                For Each Command In CommandGroup
+                    ToEcho.Append(Command.Args.Length & ", ")
+                Next
+                ToEcho.Remove(ToEcho.Length - 2, 2)
+                ToEcho.Append(" arguments")
+                ToEcho.Append(vbCrLf)
+            Next
+            Console.WriteLine(ToEcho.ToString)
+        Else
+            LogLevelDebug("Recognised input as help COMMAND")
+            LogLevelDebug("Printing Help for " & Args(0))
+            For Each CommandGroup In Parser.CommandList
+                If CommandGroup.Name = Args(0) Then
+                    LogLevelDebug("Found Match")
+                    Console.WriteLine(CommandGroup.ToString())
+                End If
+            Next
+        End If
+    End Sub
+    Public Sub DoForEcho(Args As List(Of String))
+        For Each Arg In Args
+            LogLevelDebug(Arg)
+        Next
+        If Args.Count = 1 Then
+            Console.WriteLine(Args(0))
+        ElseIf Args.Count = 2 Then
+            Console.WriteLine(Args(0))
+            For I As Integer = 0 To Integer.Parse(Args(1)) - 1
+                Console.WriteLine()
+            Next
+        End If
+    End Sub
+    Public Sub DoForDebug(Args As List(Of String))
+        Parser.Debug = Not Parser.Debug
+        ConditionalLog("Debug Mode ACTIVATED", "Debug Mode OFF")
+    End Sub
+    Public Sub DoForHide(Args As List(Of String))
+        If (Args.Count = 1) Then
+            LogLevelDebug("Recognised Pattern as FILE")
+            File.SetAttributes(Args(0), FileAttributes.Hidden)
+        ElseIf Args.Count = 2 AndAlso Args(1) = "-t" Then
+            LogLevelDebug("Recognised Pattern as FILE TOGGLE")
+            Dim attrReader As FileAttributes = File.GetAttributes(Args(0))
+            If ((attrReader And FileAttributes.Hidden) > 0) Then
+                File.SetAttributes(Args(0), FileAttributes.Normal)
+            Else
+                File.SetAttributes(Args(0), FileAttributes.Hidden)
+            End If
+        ElseIf Args.Count = 2 Then
+            LogLevelDebug("Recognised Pattern as FILE ISHIDDEN(T/F)")
+            If Boolean.Parse(Args(1)) Then
+                File.SetAttributes(Args(0), FileAttributes.Hidden)
+                LogLevelDebug("Hid File " & Args(0))
+            Else
+                File.SetAttributes(Args(0), FileAttributes.Normal)
+                LogLevelDebug("File " & Args(0) & " is not hidden anymore")
+            End If
+        End If
+    End Sub
+    Public Sub DoForCwd(Args As List(Of String))
+        Console.WriteLine(Environment.CurrentDirectory)
+    End Sub
+    Public Sub DoForCD(Args As List(Of String))
+        Try
+            Directory.SetCurrentDirectory(Args(0))
+        Catch E As Exception
+            ConditionalLog("The query :" & Args(0) & "Does not exist", "That path was not found")
+        End Try
+    End Sub
+
+
+    Public Sub DoForMkdir(Args As List(Of String))
+        If Not Args.Count = 2 Then
+            LogLevelBasic("Incorrect Number of Arguments")
+            Exit Sub
+        End If
+        If Args(0).ToLower.Equals("-dir") Then
+            Directory.CreateDirectory(Args(1))
+        Else
+            Directory.CreateDirectory(Directory.GetParent(Args(1)).FullName)
+            File.Create(Args(1)).Close()
+
+        End If
+    End Sub
+    Public Sub DoForDestroy(Args As List(Of String))
+        If (Args.Count = 1) Then
+            Try
+                If Not File.Exists(Args(0)) Then
+                    LogLevelBasic("File did Not exist in the first place!")
+                    Return
+                End If
+                File.Delete(Args(0))
+            Catch Err As Exception
+                ConditionalLog(Err.Data.ToString, "Could not delete. Try again with option debug on to see details")
+
+            End Try
+        End If
+    End Sub
+    Public Sub DoForDuplicate(ARgs As List(Of String))
+
+    End Sub
+End Module
---
+++ Module1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Text.RegularExpressions
+Imports CommandLib
+Imports System.IO
+Imports System.Reflection
+Module CommandLine
+
+    'Sub New()
+    '    Parser.CommandList.Add(New CommandGroup("help", "Displays Help") _
+    '                      .Add(New Command(AddressOf DoForHelp,
+    '                                New CommandArg("-n", "The name of the command you want information on."))) _
+    '                      .Add(New Command(AddressOf DoForHelp)))
+    '    Parser.CommandList.Add(New CommandGroup("echo", "Prints a string to the console") _
+    '                      .Add(New Command(AddressOf DoForEcho, New CommandArg("-s", "The String to be printed."))) _
+    '                      .Add(New Command(AddressOf DoForEcho, New CommandArg("-s", "The String to be printed."),
+    '                                     New CommandArg("-n", "The Amount of new lines to add after the text"))))
+    '    Parser.CommandList.Add(New CommandGroup("debug", "Toggle Parser.Debug Mode") _
+    '                      .Add(New Command(AddressOf DoForDebug)))
+    '    Parser.CommandList.Add(New CommandGroup("hide", "Hides a file") _
+    '                      .Add(New Command(AddressOf DoForHide, New CommandArg("f", "The File to hide."))) _
+    '                      .Add(New Command(AddressOf DoForHide, New CommandArg("f", "The File to hide."),
+    '                                     New CommandArg("-t", "Toggle file's HIDDEN attribute"))) _
+    '                      .Add(New Command(AddressOf DoForHide, New CommandArg("file", "The file to hide"),
+    '                                     New CommandArg("hidden", "The File's new hidden attribute"))))
+    '    Parser.CommandList.Add(New CommandGroup("cwd", "Prints Current working Directory") _
+    '                      .Add(New Command(AddressOf DoForCwd)))
+    '    Parser.CommandList.Add(New CommandGroup("cd", "Changes the CWD") _
+    '                      .Add(New Command(AddressOf DoForCD, New CommandArg("dir", "The Directory to change to"))))
+    '    Parser.CommandList.Add(New CommandGroup("create", "Creates a File of Directory") _
+    '                      .Add(New Command(AddressOf DoForMkdir, New CommandArg("-dir", "Tells the code to create a directory"),
+    '                                     New CommandArg("file", "The File to create"))) _
+    '                      .Add(New Command(AddressOf DoForMkdir, New CommandArg("-file", "Tells the code to create AddressOf File"),
+    '                                     New CommandArg("file", "The File to create"))))
+    '    Parser.CommandList.Add(New CommandGroup("delete", "Deletes a File or Directory") _
+    '                      .Add(New Command(AddressOf DoForDestroy, New CommandArg("file", "The file to erase"))))
+    '    Parser.CommandList.Add(New CommandGroup("copyto", "Copies a File or Directory to the specified location") _
+    '                      .Add(New Command(AddressOf DoForDuplicate, New CommandArg("orig", "File to copy"),
+    '                                     New CommandArg("newlocation", "The location to copy to"))) _
+    '                      .Add(New Command(AddressOf DoForDuplicate, New CommandArg("orig", "File to copy"),
+    '                                     New CommandArg("newlocation", "The location to copy to"),
+    '                                     New CommandArg("-e", "If present, deletes original after copying"))))
+
+    'End Sub
+
+    Private Plugins As New List(Of CommandLineUtilityPlugin)
+    Sub Main(Args() As String)
+        LoadPlugins()
+
+    End Sub
+    Sub LoadPlugins()
+        Dim assemblies As New List(Of Assembly)
+        Dim plugs As String = "/plugins"
+        For Each Plugin In Directory.EnumerateFiles(plugs, "*.dll")
+            Dim a = Assembly.LoadFrom(Plugin)
+            assemblies.Add(a)
+        Next
+        Dim plugType As Type = GetType(CommandLineUtilityPlugin)
+        Dim PluginTypes As New List(Of Type)
+        For Each assembly As Assembly In assemblies
+            If assembly <> Nothing Then
+                Dim cTypes As Type() = assembly.GetTypes()
+
+                For Each type As Type In cTypes
+                    If type.IsInterface Or type.IsAbstract Then
+                        Continue For
+                    Else
+                        If type.GetInterface(plugType.FullName) <> Nothing Then
+                            PluginTypes.Add(type)
+                        End If
+                    End If
+
+                Next
+            End If
+        Next
+        For Each Type In PluginTypes
+            Plugins.Add(Activator.CreateInstance(Of CommandLineUtilityPlugin))
+        Next
+    End Sub
+    Sub LogLevelDebug(Text As String)
+        If Not Parser.Debug Then
+            Return
+        End If
+        Console.WriteLine(Text)
+    End Sub
+    Sub LogLevelBasic(Text As String)
+        Console.WriteLine(Text)
+    End Sub
+    Sub ConditionalLog(IfDebug As String, IfNotDebug As String)
+        If Not Parser.Debug Then
+            LogLevelBasic(IfNotDebug)
+            Return
+        End If
+        LogLevelDebug(IfDebug)
+    End Sub
+End Module
